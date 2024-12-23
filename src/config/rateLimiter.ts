b8bf675d8145c0e75760ca57d26e221e5c86553c
megh <megh@logicrays.com>
2024-12-23 18:09:54 +0530

    Final Commit

--- src/config/rateLimiter.ts
+++ src/config/rateLimiter.ts
@@ -1,7 +1,7 @@
 import rateLimit from "express-rate-limit";
 
 export const rateLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
+  windowMs: 3 * 60 * 1000,
   max: 100,
   message: "Too many requests from this IP, please try again after 15 minutes.",
 });
